38c4e4601e79f89f6635f6428651e0fe9c22f898
mosesfuraha <moses.furaha@amalitechtraining.org>
2024-09-16 18:54:20 +0200

    create sidebar and navbar

--- src/app/components/side-bar/side-bar.component.ts
+++ src/app/components/side-bar/side-bar.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
-  styleUrl: './side-bar.component.css'
+  styleUrl: './side-bar.component.css',
 })
 export class SideBarComponent {
+  activeItem = 'Platform Launch';
 
+  isActive(item: string) {
+    return this.activeItem === item;
+  }
+
+  setActive(item: string) {
+    this.activeItem = item;
+  }
 }
